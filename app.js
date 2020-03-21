4e1a19207223b826055c7220efb2ae6f2148eaea
Vogelgesang <fvogelgesang@deloitte.de>
2020-03-21 16:32:06 +0100

    add return json to post call

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ const app = express()
 // constants
 PORT = 3000
 
+// config
+app.use(express.json())
+
 
 app.get('/', function (req, res) {
     res.send('This is the fulfillment service for the localho(r)st chatbot!')
 })
 
 app.post('/localhorst-bot-fulfilment', function (req, res) {
-    res.send('This is the fulfillment service for the localho(r)st chatbot!')
+    console.log('received post call with json')
+    res.json(req.body)
 })
 
 console.log(`running on ${PORT}`)
